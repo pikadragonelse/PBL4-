2ad3c09543c281195c9a4799727f348de5b0173b
Bảo Long <28072002long@gmail.com>
2022-10-28 18:42:57 +0700

    layout

--- messenger/src/App.js
+++ messenger/src/App.js
@@ -1,24 +1,21 @@
+import React from 'react';
+
 import './App.css';
+import { DetailInfoInbox } from './component/detail-info-inbox';
+import { FriendBox } from './component/friend-box';
+import { Inbox } from './component/inbox';
+import { Sidebar } from './component/sidebar';
+import { Wrapper } from './component/wrapper';
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <Wrapper type="wrapper-app">
+            <Sidebar />
+            <FriendBox />
+            <Inbox />
+            <DetailInfoInbox />
+        </Wrapper>
+    );
+};
 
 export default App;
---
+++ messenger/src/component/inbox/inbox.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './inbox.css';
+
+const Inbox = () => {
+    return <div className="inbox-main">inbox</div>;
+};
+
+export { Inbox };
